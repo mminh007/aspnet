d3c19b5acb3f1a91c8999d2490e2c4c59b4894a2
tuanm <tuanmminh.101112@gmail.com>
2025-08-05 21:37:26 +0700

    async/await

---
+++ AsyncDemo/AsyncDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            stopwatch.Start();
+            var t1 = Delay3();
+            var t2 = Delay4();
+
+            stopwatch.Stop();
+
+            await t1;
+            await t2;
+
+            Console.WriteLine($"Elapsed time: {stopwatch.Elapsed} ms");
+        }
+
+        public static void Delay()
+        {
+            Console.WriteLine("Delay started...");
+            Task.Delay(1000).Wait();
+            Console.WriteLine("Delay completed.");
+        }
+
+        public static void Delay2()
+        {
+            Console.WriteLine("Delay2 started...");
+            Task.Delay(2000).Wait();
+            Console.WriteLine("Delay2 completed.");
+        }
+
+        public static async Task Delay3()
+        {
+            Console.WriteLine("Delay3 started...");
+            await Task.Delay(1000);
+            Console.WriteLine("Delay3 completed.");
+        }
+
+        public static async Task Delay4()
+        {
+            Console.WriteLine("Delay4 started...");
+            await Task.Delay(2000);
+            Console.WriteLine("Delay4 completed.");
+        }
+    }
+}
