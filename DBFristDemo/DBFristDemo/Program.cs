8f0dbc89231ca9c523bc5ff6528c5c334259d133
tuanm <tuanmminh.101112@gmail.com>
2025-08-12 06:47:48 +0700

    feat(demo-dbfirst): add ASP.NET demo for Database First
    Includes EF DbContext generated from existing database schema.
    Demonstrates basic CRUD operations.

---
+++ DBFristDemo/DBFristDemo/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using DBFristDemo.Database;
+using DBFristDemo.Services;
+using DBFristDemo.Models;
+using DBFristDemo.Repository;
+
+namespace DBFristDemo
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // Register DbContext with connection string from appsettings.json
+                    var cs = context.Configuration.GetConnectionString("MyDB");
+                    services.AddDbContext<MyDbContext>(options =>
+                        options.UseSqlServer(cs));
+                    services.AddScoped<IStaffRepository, StaffRepository>();
+                    
+                    // Register other services
+                    services.AddScoped<IMyService, MyService>();
+                })
+                .Build();
+
+            // Run the application
+            using var scope = host.Services.CreateScope();
+            var myService = scope.ServiceProvider.GetRequiredService<IMyService>();
+
+            // var context = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+            Console.WriteLine("DB First Demo Application");
+            Console.WriteLine();
+
+            Console.WriteLine("List of Employees");
+            Console.WriteLine();
+            List<Employee> employees = await myService.GetAllAsync(includeNavigation: false);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.EmployeeId}, Name: {employee.FirstName} {employee.LastName}, Department: {employee.Department?.DepartmentName ?? "N/A"}, ID_Manager: {employee.ManagerId}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            Console.WriteLine("List of Employees By ManagerId");
+            Console.WriteLine("Enter Manager ID:");
+            int managerId = int.Parse(Console.ReadLine() ?? "0");
+
+            List<Employee> employeesByManager = await myService.GetEmployeesByManagerIdAsync(managerId, includeNavigation: false);
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            foreach (var employee in employeesByManager)
+            {
+                Console.WriteLine($"ID: {employee.EmployeeId}, Name: {employee.FirstName} {employee.LastName}, Department: {employee.Department?.DepartmentName ?? "N/A"}, ID_Manager: {employee.ManagerId}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            Console.WriteLine("Entered Employee ID: ");
+            int EmployeeId = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            Employee employeeById = await myService.GetByIdAsync(EmployeeId, includeNavigation: true);
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            Console.WriteLine($"ID: {employeeById.EmployeeId}, Name: {employeeById.FirstName} {employeeById.LastName}, " +
+                                                            $"Department: {employeeById.Department?.DepartmentName ?? "N/A"}, " +
+                                                            $"Job: {employeeById.Job.JobTitle}, " +
+                                                            $"Manager: {employeeById.Manager?.FirstName} {employeeById.Manager?.LastName}");
+
+        }
+    }
+}
---
+++ DBFristDemo/DBFristDemo/Repository/StaffRepository.cs
@@ -0,0 +1,201 @@
+﻿using DBFristDemo.Database;
+using DBFristDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFristDemo.Repository
+{
+    public class StaffRepository : IStaffRepository
+    {
+
+        private readonly MyDbContext _context;
+
+        public StaffRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Employee> AddAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            return employee;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            await _context.Employees
+                .Where(e => e.EmployeeId == id)
+                .ExecuteDeleteAsync();
+            return true;
+        }
+
+        public async Task<List<Employee>> GetAllAsync(bool includeNavigation = false)
+        {
+            
+            if (includeNavigation)
+            {
+                return await _context.Employees
+                    .Include(e => e.Department)
+                    .Include(e => e.Job)
+                    .Include(e => e.Manager)
+                    .ToListAsync();
+            }
+            else
+            {
+                return await _context.Employees.ToListAsync();
+            }
+
+        }
+
+        public async Task<Employee> GetByIdAsync(int id, bool includeNavigation = false)
+        {
+            IQueryable<Employee> query = _context.Employees.AsNoTracking();
+
+            if (includeNavigation)
+            {
+                query = query
+                    .Include(e => e.Department)
+                    .Include(e => e.Job)
+                    .Include(e => e.Manager);
+                
+            }
+
+            var emp = await query
+                .Where(e => e.EmployeeId == id)
+                .FirstOrDefaultAsync();
+
+            if (emp == null)
+                {
+                throw new KeyNotFoundException($"Employee with ID {id} not found.");
+            }
+            return emp;
+        }
+
+        public async Task<List<Dependent>> GetDependentsByEmployeeIdAsync(int employeeId, bool includeNavigation = false)
+        {
+            
+            if (includeNavigation)
+            {
+                return await _context.Dependents
+                    .Include(d => d.Employee)
+                    .Where(d => d.EmployeeId == employeeId)
+                    .ToListAsync();
+            }
+            else
+            {
+                return await _context.Dependents
+                    .Where(d => d.EmployeeId == employeeId)
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<List<Employee>> GetEmployeesByDepartmentIdAsync(int departmentId, bool includeNavigation = false)
+        {
+            if (includeNavigation)
+            {
+                return await _context.Employees
+                    .Include(e => e.Department)
+                    .Include(e => e.Job)
+                    .Include(e => e.Manager)
+                    .Where(e => e.DepartmentId == departmentId)
+                    .ToListAsync();
+            }
+            else
+            {
+                return await _context.Employees
+                    .Where(e => e.DepartmentId == departmentId)
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<bool> IsManagerAsync(int id)
+        {
+            return await _context.Employees
+                .AnyAsync(e => e.ManagerId == id);
+        }
+
+        public async Task<bool> HasManagerAsync(int id)
+        {
+            return await _context.Employees
+                .AnyAsync(e => e.EmployeeId == id && e.ManagerId != null);
+        }
+
+        public async Task<bool> EmployeeExistsAsync(int id)
+        {
+             return await _context.Employees.AnyAsync(e => e.EmployeeId == id);
+
+        }
+            
+
+        public async Task<List<Employee>> GetEmployeesByManagerIdAsync(int managerId, bool includeNavigation = false)
+        {   
+
+            if (!await EmployeeExistsAsync(managerId))
+            {
+                Console.WriteLine();
+                Console.WriteLine(new string('-', 50));
+                Console.WriteLine();
+                Console.WriteLine($"Employee with ID {managerId} not found.");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            bool isManager = await IsManagerAsync(managerId);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            if (!isManager)
+            {
+                Console.WriteLine($"Manager with ID {managerId} not found.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+
+            if (includeNavigation)
+            {
+                return await _context.Employees
+                    .Include(e => e.Department)
+                    .Include(e => e.Job)
+                    .Include(e => e.Manager)
+                    .Where(e => e.ManagerId == managerId)
+                    .ToListAsync();
+            }
+            else
+            {
+                return await _context.Employees
+                    .Where(e => e.ManagerId == managerId)
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<Employee> GetManagerByIdAsync(int id, bool includeNavigation = false)
+        {
+            var manager = await GetByIdAsync(id, includeNavigation);
+            return manager;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Employee> UpdateAsync(Employee employee)
+        {
+            return await Task.Run(() =>
+            {
+                _context.Employees.Update(employee);
+                return employee;
+            });
+        }
+    }
+}
